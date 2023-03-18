ac7397baf1ab188e575693b60d30966a10d3cba8
Obie <obiemunozjr@gmail.com>
2023-03-17 21:14:34 -0400

    New method to determine if a channel is live

---
+++ src/lib/TwitchUtils.ts
@@ -0,0 +1,15 @@
+export async function checkLive(channel: string): Promise<boolean> {
+	try {
+		const url = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${channel}-1x1.jpg`;
+		const response = await fetch(url, { redirect: 'manual' });
+		if (response.status === 200) {
+			return true; // Channel is live
+		} else if (response.status === 404) {
+			return false; // Channel is offline
+		} else {
+			throw new Error('Unexpected status code: ' + response.status);
+		}
+	} catch (error) {
+		return false; // Error occurred, assume channel is offline
+	}
+}
